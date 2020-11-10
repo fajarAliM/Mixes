840ab11c845393e303458108e07db52f38a7ac58
M Fajar Ali <fajaralim678@gmail.com>
2020-11-10 16:12:07 -0600

    remove microsoft stuff

--- pages/index.js
+++ pages/index.js
@@ -33,8 +33,6 @@ export default function IndexPage() {
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
 				<link rel="manifest" href="/site.webmanifest">
-				<meta name="msapplication-TileColor" content="#da532c">
-				<meta name="theme-color" content="#ffffff">
 			</Head>
 			<div className="w-full">
 				<h1 className="font-bold text-5xl text-center text-accent-1 mb-4">
