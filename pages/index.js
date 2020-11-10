6352f1c2f62d5042f3ebbbb998a7e2d08b94bdf8
M Fajar Ali <fajaralim678@gmail.com>
2020-11-10 16:16:31 -0600

    Removing some more stuff

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,6 @@ export default function IndexPage() {
 				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
 				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-				<link rel="manifest" href="/site.webmanifest">
 			</Head>
 			<div className="w-full">
 				<h1 className="font-bold text-5xl text-center text-accent-1 mb-4">
